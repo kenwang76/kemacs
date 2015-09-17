aede976615f2036b2c0b55e310625a53710c3263
Ken Wang <kenwang@kakitangan.com>
2015-09-17 12:21:08 +0800

    add more configs from http://sachin.pythonanywhere.com/

--- _emacs/init.el
+++ _emacs/init.el
@@ -1,23 +1,35 @@
-;; company-mode
-(add-hook 'after-init-hook 'global-company-mode)
+;; ======================================================================
+;; INSTRUCTIONS: This file is auto-generated from Haqiba(haqiba.org).
+;; Untar this tarball(emacs.d.tar) using the command
+;; 'tar -xvf emacs.d.tar'
+;; and copy the directory '.emacs.d' to your $HOME(~/).
+;; ======================================================================
 
-;;add custom theme path
-(load-theme 'wombat t)
+;;; Package
+;; Note: This configuration may not work with old versions of Emacs.
+;; Marmalade repo
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
-;;enable IDO
-(require 'ido)
-(ido-mode t)
+;; Melpa repo
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
-;; make ido display choices vertically
-(setq-default ido-separator "\n")
+;;; Column-number
+;; Show column number in mode-line.
+(column-number-mode t)
 
-;; display any item that contains the chars you typed
-(setq-default ido-enable-flex-matching t)
+;;; No-tool-menu-bar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
 
-;;use spaces instead of tabs
-(setq-default indent-tabs-mode nil)
+;;; Server
+(require 'server)
+(unless (server-running-p)
+  (server-start))
 
-;;tab width: 4
-(setq-default tab-width 4)
-
+;;; Which-function
+(which-function-mode t)
 
