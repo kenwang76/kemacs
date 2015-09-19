5e5e4ca7a4259a01c29b3b4b4d9c0b8ae1a7eb50
Ken Wang <kenwang@kakitangan.com>
2015-09-19 21:16:23 +0800

    remove packages related configs

--- _emacs/init.el
+++ _emacs/init.el
@@ -5,18 +5,6 @@
 ;; and copy the directory '.emacs.d' to your $HOME(~/).
 ;; ======================================================================
 
-;;; Package
-;; Note: This configuration may not work with old versions of Emacs.
-;; Marmalade repo
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;; Melpa repo
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-
 ;; company-mode
 (add-hook 'after-init-hook 'global-company-mode)
 
